f17bf5b70fdd4b57508faba311ca443add4fb926
Ishrz <ishrzz66@gmail.com>
2025-12-16 19:38:52 +0530

    WIP: added pages and back button logic

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 let allElems= document.querySelectorAll('.elem')
-console.log(allElems)
+let allFullElem=document.querySelectorAll('.fullElem')
+let allFullElemBackBtn=document.querySelectorAll('.fullElem .back')
+
+allElems.forEach(elem =>{
+    elem.addEventListener('click', (evt)=>{
+        allFullElem[elem.id].style.display='block'
+    })
+
+    
+})
+
+allFullElemBackBtn.forEach(elem=>{
+    elem.addEventListener("click",()=>{
+        // console.log(elem.id)
+    allFullElem[elem.id].style.display='none'
+    })
+})
+
+// allFullElemBackBtn.addEventListener('click',()=>{
+//     console.log(back)
+// })
