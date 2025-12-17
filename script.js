7cc949aec3b2ced7962d81798a0a219c0060c07d
Ishrz <ishrzz66@gmail.com>
2025-12-17 18:41:29 +0530

    formated the syntax

--- script.js
+++ script.js
@@ -24,68 +24,59 @@ let input = document.querySelector(".addTask form #task-input");
 let textArea = document.querySelector(".addTask form textarea");
 let taskCheckBox = document.querySelector(".addTask form #check");
 
-let currentTask = [];
-let savedData=localStorage.getItem('currentTask')
-
-if(savedData){
-  currentTask=JSON.parse(savedData)
-}else{
-  console.log('localStorage is empty')
-}
 
 
+function todoList() {
+  let currentTask = [];
+  let savedData = localStorage.getItem("currentTask");
 
-// localStorage.clear()
+  if (savedData) {
+    currentTask = JSON.parse(savedData);
+  } else {
+    console.log("localStorage is empty");
+  }
 
+  // localStorage.clear()
 
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
 
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  if(input.value && textArea.value){
-  currentTask.push({
-    task: input.value,
-    detail: textArea.value,
-    check: taskCheckBox.checked,
+    if (input.value && textArea.value) {
+      currentTask.push({
+        task: input.value,
+        detail: textArea.value,
+        check: taskCheckBox.checked,
+      });
+    }
+    renderTask();
+    input.value = "";
+    textArea.value = "";
+    taskCheckBox.checked = false;
   });
-}
-  renderTask();
-  input.value = "";
-  textArea.value = "";
-  taskCheckBox.checked = false;
-});
 
-
-
-function renderTask() {
-  let allTask = document.querySelector(".allTask");
-  let data = "";
-  currentTask.forEach((elem,index) => {
-    data += `<div class="task">
+  function renderTask() {
+    let allTask = document.querySelector(".allTask");
+    let data = "";
+    currentTask.forEach((elem, index) => {
+      data += `<div class="task">
                     <h5>${elem.task} <span class=${elem.check}>imp</span></h5>
                     <button id=${index}>mark compeleted</button>
                 </div>`;
-  });
+    });
 
-  
-  console.log(currentTask)
-  localStorage.setItem('currentTask',JSON.stringify(currentTask))
-  allTask.innerHTML = data;
-  console.log(currentTask)
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    allTask.innerHTML = data;
 
+    let allTaslBtn = document.querySelectorAll(".task button");
+    allTaslBtn.forEach((btn) => {
+      btn.addEventListener("click", (evt) => {
+        currentTask.splice(currentTask[btn.id], 1);
+        renderTask();
+      });
+    });
+  }
 
-  let allTaslBtn=document.querySelectorAll('.task button')
-  allTaslBtn.forEach(btn=>{
-    btn.addEventListener('click',(evt)=>{
-      console.log(savedData)
-      console.log(btn.id)
-      console.log(currentTask[btn.id])
-
-      currentTask.splice(currentTask[btn.id],1)
-      renderTask()
-    })
-  })
+  renderTask();
 }
 
-renderTask();
-
+todoList()
